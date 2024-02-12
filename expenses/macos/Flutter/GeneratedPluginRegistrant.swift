13311da5baba7ab33c105a212856d6313280e04c
santhoshm088 <115387017+santhoshm088@users.noreply.github.com>
2024-02-12 14:20:53 +0530

    first

--- expenses/macos/Flutter/GeneratedPluginRegistrant.swift
+++ expenses/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import flutter_udid
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterUdidPlugin.register(with: registry.registrar(forPlugin: "FlutterUdidPlugin"))
 }
